4c277b517eccec83046cef21f5bf34583ba041b2
yanhao.xu <yanhao.xu@outlook.com>
2023-07-07 00:02:46 +0800

    erabbit vue3pre learning

---
+++ vue3-pre/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Setup />
+</template>
+
+<script>
+import Setup from "./components/Setup.vue";
+
+export default {
+  name: "App",
+  components: {
+    Setup,
+  },
+};
+</script>
---
+++ vue3-pre/src/components/Setup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1 @click="say()">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    console.log("setup 执行了");
+    console.log(this);
+
+    const msg = "hi vue3";
+
+    const say = () => {
+      console.log(msg);
+    };
+
+    return { msg, say };
+  },
+  beforeCreate() {
+    console.log("beforeCreate执行了");
+    console.log(this);
+  },
+};
+</script>
