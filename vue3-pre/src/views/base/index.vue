cd2a26dd3e064d9c503f21fa6863edf6f12bca74
yanhao.xu <yanhao.xu@outlook.com>
2023-07-18 00:23:39 +0800

    胡须语法

--- vue3-pre/src/views/base/index.vue
+++ vue3-pre/src/views/base/index.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
     <h1>基本語法</h1>
+    <p>我是p标签</p>
+    <h3>胡须语法</h3>
+    <h3>num:{{ num1 }}</h3>
+    <h3>函数:{{ getMsg() }}</h3>
+    <h3>四则运算:{{ num1 + num2 }}</h3>
+    <h3>三目运算运算:{{ false ? 40 : 60 }}</h3>
+    <h3>短路运算符:{{ true && "你好啊" }}</h3>
+    <h3>正则表达式:{{ /www/.test("wwwww") }}</h3>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+// 胡须语法 {{表达式}}
+const num1 = 20;
+function getMsg() {
+  return "hello";
+}
+// 四则运算
+const num2 = 30;
+</script>
 
 <style lang="scss" scoped></style>
