588dae6a97c2a3faed1e7f90fd2cabbb6e6d56d6
yanhao.xu <yanhao.xu@outlook.com>
2023-09-09 00:18:14 +0800

    expose

---
+++ vue3-pre/src/views/expose/Child1.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>子组件</h3>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+// options语法 推荐卸载defineComponent里面
+// options语法 组件数据和方法是默认暴露出去的
+export default defineComponent({
+  data() {
+    return {
+      msg: "你好啊",
+    };
+  },
+  methods: {
+    getMsg() {
+      console.log(this.msg);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue3-pre/src/views/expose/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>父组件</h1>
+    <Child1 ref="child1Ref" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import Child1 from "./Child1.vue";
+
+// 组合式api语法 推荐写在setup语法糖里面
+const child1Ref = ref(null);
+
+onMounted(() => {
+  console.log(child1Ref);
+  console.log("child1Ref.value.msg :>> ", child1Ref.value.msg);
+  child1Ref.value.getMsg();
+});
+</script>
+
+<style lang="scss" scoped></style>
